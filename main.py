9851e31b5f6a531e241bffbefb2fa5bb431fde22
David <davidnathanrose@gmail.com>
2025-01-19 22:37:36 +0000

    further work to tidying data extraction workflow

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import verbatim as vb
 print("Please select a mode for verbatim")
 print("1: Convert new PDF documents to images for bounding box annotation")
 print("2: Extract text from PDF documents which have been annotated")
-print("3: Convert extracted text to audio file")
-mode_select = input("Select Mode (1 / 2 / 3)")
+# print("3: Convert extracted text to audio file")
+mode_select = input("Select Mode (1 / 2)")
+
+# TODO: add a master table to the database for faster lookup
 
 if mode_select == '1':
     # convert new pdfs to images for further processing
@@ -16,5 +18,4 @@ if mode_select == '2':
     vb.img_to_txt()
 if mode_select == '3':
     # convert text to audio and save various metadata
-    pass
 
